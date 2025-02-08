52f838f849acac8430f77364af1f198f0b0666e5
Emerssongit <emerssonbarbosaramos@gmail.com>
2025-02-08 19:44:56 +0000

    inicio

--- CartaSuperTrunfo.c/DesafioSuperTrunfo.c
+++ CartaSuperTrunfo.c/DesafioSuperTrunfo.c
@@ -2,15 +2,22 @@
 
 int main(){
 
-    printf("Desfio Supertrunfo\n");
+    printf("Brasil\n");
 
-    return 0;
+    int codigo = 01;
+    char nome[20] = "Brasil";
+    float populacao = 216.4;
+    float area = 8.510000;
+    float pib = 2.174000000000;
+    float pontosturisticos = 0;
+
+    printf(" Qual o codigo da cidade ?\n");
+    scanf(" %d\n", &codigo);
+    scanf("resposta correta 01\n");
+
+   
 
     
-
-
-
-
 
 
 }
