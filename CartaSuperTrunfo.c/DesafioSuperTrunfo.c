2541bdd344ae43f47f376c1fdab44b030f030762
Emerssongit <emerssonbarbosaramos@gmail.com>
2025-02-11 00:26:51 +0000

    Novo trabalho CartaSuperTrunfo

--- CartaSuperTrunfo.c/DesafioSuperTrunfo.c
+++ CartaSuperTrunfo.c/DesafioSuperTrunfo.c
@@ -1,29 +1,47 @@
 #include <stdio.h>
 
 int main(){
-    //Variaveis para amazenar os nomes das cidade.
-     char estado;
-     int codigo_cidade; //Exemplo 1,2,3,4 ou A01,A02...
-     char cidade [60]; //Nome da cidade;
-     float populacao;
-     float area;
-     float pib;
-     float pontos_turisticos;
 
-     printf("Digite o nome do estado: \n");
-     scanf(" %s/n", &estado);
+    //Variaveis com nome do estado, cidade, codigo, are, pib...
+    int numero,codigo; //Numero carta 1,2,3,4.
+    char nome[130]; //Nome do estado e cidade;
+    float pib;   //População;
+    float area;  //Area km2;
+    float pontos_turisticos;
 
-     printf("Digite o codigo da carta: \n");
-     scanf(" %d\n", &codigo_cidade);
+    //Printf -> Comando para ser impresso na tela e scanf para ler dados do teclado e atribui as variaveis.
 
+    printf("Codigo do estado da carta: \n"); 
+    scanf( " %d\n", &codigo);
 
-   
+    printf("Numeração da carta: \n");
+    scanf( "%d\n", &numero); //Aqui tive que dar espaço entre parenteses e aspas pois estava dando erro de espaçamento.
 
-    
+    printf("Nome estado: \n");
+    scanf( "%s\n", &nome);
 
-   
+    printf("Nome cidade: \n");
+    scanf( "%s\n", &nome);
 
-    
+    printf("Pib da cidade: \n");
+    scanf("%c\n", &pib);
+
+    printf("Área da cidade: \n");
+    scanf("%c\n", &area);
+
+    printf("Numero de pontos turisticos: \n");
+    scanf("%c\n ", &pontos_turisticos);
+
+    printf("Codigo do estado da carta: %d\n", &codigo);
+    printf("Numeração da carta: %d\n", &numero);
+    printf("Nome estado: %s\n",&nome);
+    printf("Nome cidade: %s\n", &nome);
+    printf("Pib da cidade: %c\n", &pib);
+    printf("Área da cidade: %c\n", &area);
+    printf("Numero de pontos turisticos: %c\n", &pontos_turisticos);
+
+    return 0;
+
 
 
 }
