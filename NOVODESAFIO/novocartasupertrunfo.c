f62d160ed5c09d54fef6aece8329b0d020f81b4b
Emerssongit <emerssonbarbosaramos@gmail.com>
2025-02-18 15:26:00 +0000

    CARTASUPERTRUNFO.C

--- NOVODESAFIO/novocartasupertrunfo.c
+++ NOVODESAFIO/novocartasupertrunfo.c
@@ -1,4 +1,9 @@
-#include <stdio.h>
+#include <stdio.h> // Inclui a biblioteca padrão de entrada e saída em C
+
+void LimparBuffer() {                                    // Essa função limpa o buffer.
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);       
+}
 
 int main() {
     //Nome das variaveis com estado, cidade, codigo/número, pib, km2...
@@ -12,36 +17,51 @@ int main() {
 
     //Leitura dos codigos cadastrados
 
-    printf("Codigo da carta (ex: A01, B02):");
+    printf("Codigo da carta (ex: A01, B02): \n");
     scanf("%s", codigo);
-
-    printf("Nome do estado:");
-    scanf("%s", estado);
-
-    printf("Nome da cidade:");
-    scanf("%s", nome);
-
-    printf("População da cidade:");
-    scanf(" %f[^\n]", &populacao); // Usei fgets para ler a strings, pois estava pulando linha sem pegar dados do scanf.
-
-    printf("Área da cidade (km2):");
-    scanf(" %f[^\n]", &area);
-
-    printf("PIB da cidade:");
-    scanf(" %f[^\n]", &pib);
-
-    printf("Número de pontos turísticos:");
-    scanf(" %f[^\n]", &pontos_turisticos);
-
-    // Leitura dos dados cadastrado pelo usuario x de cada scanf.
+    LimparBuffer();
     
-    printf("Codigo da carta: %s", codigo);
-    printf("Nome do estado: %s \n", estado);
-    printf("Nome da cidade: %s \n", nome);
-    printf("População da cidade: %f \n", populacao);
-    printf("Área da cidade (km2): %f", area);
-    printf("PIB da cidade: %f \n", pib);
+
+    printf("Nome do estado: \n");
+    scanf("%s", estado);
+    LimparBuffer();
+    
+
+    printf("Nome da cidade: \n");
+    scanf("%s", nome);
+    LimparBuffer();
+ 
+    printf("População da cidade: \n");
+    scanf("%f", &populacao);
+    LimparBuffer();
+    
+
+    printf("Área da cidade (km2): \n");
+    scanf("%f", &area);
+    LimparBuffer();
+    
+
+    printf("PIB da cidade: \n");
+    scanf("%f", &pib);
+    LimparBuffer();
+
+
+    printf("Número de pontos turísticos: \n");
+    scanf("%f", &pontos_turisticos);
+    LimparBuffer();
+
+
+    // Leitura dos dados cadastrado pelo usuario x de cada scanf. 
+    printf("Codigo da carta: %s \n", codigo); 
+    printf("Nome do estado: %s \n", estado); 
+    printf("Nome da cidade: %s \n", nome); 
+    printf("População da cidade: %f \n", populacao); 
+    printf("Área da cidade (km2): %f \n", area); 
+    printf("PIB da cidade: %f \n", pib); 
     printf("Número de pontos turísticos: %f \n", pontos_turisticos);
+
+ 
+
 
     return 0;
 
