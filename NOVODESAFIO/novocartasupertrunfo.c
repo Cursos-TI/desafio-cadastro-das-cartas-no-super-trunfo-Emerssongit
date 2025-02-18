39734d45b6558aa52076df874f11912018afebcd
Emerssongit <emerssonbarbosaramos@gmail.com>
2025-02-18 00:11:55 +0000

    cartasupertrunfo

--- NOVODESAFIO/novocartasupertrunfo.c
+++ NOVODESAFIO/novocartasupertrunfo.c
@@ -13,25 +13,37 @@ int main() {
     //Leitura dos codigos cadastrados
 
     printf("Codigo da carta (ex: A01, B02):");
-    scanf("%s", &codigo);
+    scanf("%s", codigo);
 
-    printf("Nome do estado: ");
+    printf("Nome do estado:");
     scanf("%s", estado);
 
-    printf("Nome da cidade: ");
+    printf("Nome da cidade:");
     scanf("%s", nome);
 
-    printf("População da cidade: ");
-    scanf(" %f[^\n]", &populacao); //Usei fgets para ler a strings, resolvendo problema de pular linhas.
+    printf("População da cidade:");
+    scanf(" %f[^\n]", &populacao); // Usei fgets para ler a strings, pois estava pulando linha sem pegar dados do scanf.
 
-    printf("Área da cidade (km2): ");
-    scanf("%f", &area);
+    printf("Área da cidade (km2):");
+    scanf(" %f[^\n]", &area);
 
-    printf("PIB da cidade: ");
-    scanf("%f", &pib);
+    printf("PIB da cidade:");
+    scanf(" %f[^\n]", &pib);
 
-    printf("Número de pontos turísticos: ");
-    scanf("%f", &pontos_turisticos);
+    printf("Número de pontos turísticos:");
+    scanf(" %f[^\n]", &pontos_turisticos);
+
+    // Leitura dos dados cadastrado pelo usuario x de cada scanf.
+    
+    printf("Codigo da carta: %s", codigo);
+    printf("Nome do estado: %s \n", estado);
+    printf("Nome da cidade: %s \n", nome);
+    printf("População da cidade: %f \n", populacao);
+    printf("Área da cidade (km2): %f", area);
+    printf("PIB da cidade: %f \n", pib);
+    printf("Número de pontos turísticos: %f \n", pontos_turisticos);
+
+    return 0;
 
 
 }
