c010cb2603a21707b9f3a7ba89d86bdbd459ae78
Emerssongit <emerssonbarbosaramos@gmail.com>
2025-02-20 14:46:45 +0000

    SUPER_TRUNFO

--- CartaSuperTrunfo.c/SUPER_TRUNFO.C/supertrunfo.c
+++ CartaSuperTrunfo.c/SUPER_TRUNFO.C/supertrunfo.c
@@ -4,21 +4,19 @@
 
 int main() {
     
-    char estado[90]; // Variavel com nome estado.
-    char codigo[5];  // Variavel com codigo carta.
-    char cidade[90]; // Variavel com nome da cidade.
-    int populacao; // Variavel com número da população.
-    float area; // variavel, área da cidade.
-    float pib;  // variavel, PIB da cidade.
-    int numero; // Variavel com número de pontos turisticos.
+    char estado[90], estado2[90]; // Variavel com nome estado.
+    char codigo[5], codigo2[5];  // Variavel com codigo carta.
+    char cidade[90], cidade2[90]; // Variavel com nome da cidade.
+    float populacao, populacao2; // Variavel com número da população.
+    float area, area2; // variavel, área da cidade.
+    float pib, pib2;  // variavel, PIB da cidade.
+    int numero, numero2; // Variavel com número de pontos turisticos.
 
     // Leitura e caputra de dados das cartas.
 
-    // Em todos scanf antes dos especificadores coloquei espaço para não dar erro pulando linha.
-
     printf("Codigo: 1\n");
     printf("Estado: ");
-    scanf(" %s", estado);  // Aqui e em todas as strings não usei o '&', como é uma string o scanf amarzena o caracter sem '&'.
+    scanf(" %s", estado); 
 
     printf("Codigo da carta: ");
     scanf(" %s", codigo);
@@ -27,7 +25,7 @@ int main() {
     scanf(" %s", cidade);
 
     printf("População: ");
-    scanf(" %i", &populacao);
+    scanf(" %f", &populacao);
 
     printf("Área km²: ");
     scanf(" %f", &area);
@@ -44,7 +42,7 @@ int main() {
     printf("Estado: %s\n", estado);
     printf("Codigo da carta: %s\n", codigo);
     printf("Nome da cidade: %s\n", cidade);
-    printf("População: %d\n", populacao);
+    printf("População: %f\n", populacao);
     printf("Área km²: %f\n", area);
     printf("PIB: %f\n", pib);
     printf("Número de Pontos Turísticos: %i\n", numero);
@@ -53,25 +51,25 @@ int main() {
 
     printf("\nCarta: 2\n");
     printf("Estado: ");
-    scanf(" %s", estado);
+    scanf(" %s", estado2);
 
     printf("Codigo da carta: ");
-    scanf(" %s", codigo);
+    scanf(" %s", codigo2);
 
     printf("Nome da cidade:");
-    scanf(" %s", cidade);
+    scanf(" %s", cidade2);
 
     printf("População: ");
-    scanf(" %d", &populacao);
+    scanf(" %f", &populacao2);
 
     printf("Área km²: ");
-    scanf(" %f", &area);
+    scanf(" %f", &area2);
 
     printf("PIB: ");
-    scanf(" %f", &pib);
+    scanf(" %f", &pib2);
 
     printf("Número de Pontos Turísticos: ");
-    scanf(" %i", &numero);
+    scanf(" %i", &numero2);
 
     // Leitura de dados cadastrados pelo usuarios.
 
